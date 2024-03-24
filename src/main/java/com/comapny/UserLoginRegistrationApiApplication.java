6927edfc4cd4acdea9abeea18eb20f040c4e4b3b
Sebastian Lachut <seb.lachut@gmail.com>
2024-03-24 11:42:10 +0100

    Initial commit

---
+++ src/main/java/com/comapny/UserLoginRegistrationApiApplication.java
@@ -0,0 +1,13 @@
+package com.comapny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginRegistrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserLoginRegistrationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comapny/UserLoginRegistrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comapny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLoginRegistrationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
