c4766768e56751715bcf999e4a5f8b9d7f0bc310
Sebastian Lachut <seb.lachut@gmail.com>
2024-03-24 12:20:44 +0100

    Created db connection as well as AppUser class

---
+++ src/main/java/com/comapny/Messages.java
@@ -0,0 +1,5 @@
+package com.comapny;
+
+public class Messages {
+    public final static String USER_NOT_FOUND_MSG = "User with email %s not found";
+}
---
+++ src/main/java/com/comapny/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.comapny.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
