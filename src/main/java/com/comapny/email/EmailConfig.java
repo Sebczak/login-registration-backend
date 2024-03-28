b3be53f3d2a6cd2b47528b8b8d686a41f5d09f6e
Sebastian Lachut <seb.lachut@gmail.com>
2024-03-28 22:27:17 +0100

    Created implementation for sending emial to user when registering

---
+++ src/main/java/com/comapny/email/EmailConfig.java
@@ -0,0 +1,29 @@
+package com.comapny.email;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("your_mail_host");
+        mailSender.setPort(587);
+        mailSender.setUsername("your_username");
+        mailSender.setPassword("your_password");
+
+        Properties props = mailSender.getJavaMailProperties();
+        ((Properties) props).put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
