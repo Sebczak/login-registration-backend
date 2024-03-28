40e36c85360df08ecaefa1aca28eb7e4d6451feb
Sebastian Lachut <seb.lachut@gmail.com>
2024-03-28 21:58:16 +0100

    Create ConfirmationToken that lets registered user login

--- src/main/java/com/comapny/Messages.java
+++ src/main/java/com/comapny/Messages.java
@@ -4,4 +4,7 @@ public class Messages {
     public final static String USER_NOT_FOUND_MSG = "User with email %s not found";
     public final static String EMAIL_NOT_VALID = "Email not valid";
     public static final String EMAIL_ALREADY_TAKEN = "Email already taken";
+    public static final String TOKEN_NOT_FOUND = "Token not found";
+    public static final String EMAIL_ALREADY_CONFIRMED = "Email already confirmed";
+    public static final String TOKEN_CONFIRMED = "Token confirmed";
 }
